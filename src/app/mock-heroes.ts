600fbdfc0f791a6de67a85b0e0624417af573a24
sugarrxx <sugarrxx@outlook.com>
2019-03-03 19:20:37 +0800

    AngularDemo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: '影流之主' },
+  { id: 12, name: '诺克萨斯之手' },
+  { id: 13, name: '虚空之女' },
+  { id: 14, name: '正义巨像' },
+  { id: 15, name: '荣耀行刑官' },
+  { id: 16, name: '牛头酋长' },
+  { id: 17, name: '刀锋之影' },
+  { id: 18, name: '探险家' },
+  { id: 19, name: '炼金术士' },
+  { id: 20, name: '迅捷斥候' }
+];
